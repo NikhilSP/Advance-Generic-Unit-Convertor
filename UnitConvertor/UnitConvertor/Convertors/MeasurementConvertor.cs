33bc1050712de87a7f2216bad994cc197213b6ce
Nikhil Pundir <npundir@cerebre.io>
2025-01-08 18:44:32 -0600

    feat : implement simple unit convertor

--- UnitConvertor/UnitConvertor/Convertors/MeasurementConvertor.cs
+++ UnitConvertor/UnitConvertor/Convertors/MeasurementConvertor.cs
@@ -1,15 +1,62 @@
 
 using UnitConvertor.Contract;
 using UnitConvertor.Model.Measurement;
+using UnitConvertor.Model.Unit;
 
 namespace UnitConvertor.Convertors;
 
 public class MeasurementConvertor
 {
+    private readonly Dictionary<(Type,Type),Func<double,double>> _lengthConverters = new()
+    {
+        {(typeof(Meter), typeof(Feet)), v => v * 3.28084},
+        {(typeof(Feet), typeof(Meter)), v => v * 0.3048},
+    };
+    
+    private readonly Dictionary<(Type,Type),Func<double,double>> _temperatureConverters = new()
+    {
+        {(typeof(Celsius), typeof(Fahrenheit)), v => (v * 9/5) + 32},
+        {(typeof(Fahrenheit), typeof(Celsius)), v => (v - 32) * 5/9}
+    };
+    
+    private readonly Dictionary<(Type,Type),Func<double,double>> _weightConverters = new()
+    {
+        {(typeof(Gram), typeof(Pound)), v => v * 2.20462},
+        {(typeof(Pound), typeof(Gram)), v => v * 0.453592}
+    };
+    
     public Measurement<T2> ConvertTo<T1, T2>(Measurement<T1> measurement)
         where T1 :  IUnit 
         where T2 :  IUnit
     {
-        return null;
+        var convertors = GetConvertors<T1,T2>();
+
+        if (convertors.Any())
+        {
+            if (convertors.TryGetValue((typeof(T1), typeof(T2)), out var func))
+            {
+                return new Measurement<T2>(func(measurement.Value));
+            }
+        }
+        
+        return new NotDefinedMeasurement<T2>();
+    }
+
+    private Dictionary<(Type, Type), Func<double, double>> GetConvertors<T1,T2>()
+    {
+        if (typeof(ILength).IsAssignableFrom(typeof(T1)) && typeof(ILength).IsAssignableFrom(typeof(T2)))
+        {
+            return _lengthConverters;
+        }
+        else  if (typeof(IWeight).IsAssignableFrom(typeof(T1)) && typeof(IWeight).IsAssignableFrom(typeof(T2)))
+        {
+            return _weightConverters;
+        }
+        else  if (typeof(ITemperature).IsAssignableFrom(typeof(T1)) && typeof(ITemperature).IsAssignableFrom(typeof(T2)))
+        {
+            return _temperatureConverters;
+        }
+
+        return new();
     }
 }
--- UnitConvertor/UnitConvertor/Program.cs
+++ UnitConvertor/UnitConvertor/Program.cs
@@ -1,4 +1,5 @@
-﻿using UnitConvertor.Model.Measurement;
+﻿using UnitConvertor.Convertors;
+using UnitConvertor.Model.Measurement;
 using UnitConvertor.Model.Unit;
 
 namespace UnitConvertor;
@@ -8,5 +9,10 @@ internal class Program
     static void Main(string[] args)
     {
         var first = new Measurement<Meter>(10);
+
+        var convertor = new MeasurementConvertor();
+
+        var result = convertor.ConvertTo<Meter, Feet>(first);
+
     }
 }
